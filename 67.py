d76f7f36a6542a820784f6eec0b6727725898f67
uuracr <acarugur66@gmail.com>
2025-07-06 12:56:55 +0300

    Add files via upload

---
+++ 67.py
@@ -0,0 +1,30 @@
+a = "Ankara"
+b = "Vektörel"
+c = a+b
+print(c)
+c = f"(a) ile (b)"
+print(f" (a) ile (b)")
+c ="() ile2 ()".format(a,b) 
+print(c)
+print(a[0])
+print(b[2:5])
+print(b[-2:])
+print(a[-3:])
+meyveler = ["Muz", "Elma", "Nar"]
+print(meyveler)
+print(meyveler[1])
+print(meyveler[2])
+print(b[:])
+sayi1 = input("1.sayıyı gir:")
+sayi2 = input("2. sayıyı gir:")
+sonuc = (sayi1+sayi2)
+print(sonuc)
+sayi1 = input("1.sayıyı gir:")
+sayi2 = input("2.sayıyı gir:")
+sonuc = int(sayi1)+int(sayi2)
+print(sonuc)
+
+ad = input ("Adın ne?")
+print(f"Adın {ad} demek. Çok güzel bir isim.")
+dy = int(input(f"Hangi yılda doğdun {ad}."))
+print(f"Demek {2025-dy} yaşındasın {ad}. ")         
